163060c35b2a82d24ca65d5a9e8e1c7913eeeff7
Howard Chan <guanhao3797@gmail.com>
2014-09-20 21:45:06 +0800

    Added working cpp version

---
+++ cash.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+int *start_ptr = new int[1000000];
+
+int make_hash(string key) {
+  int hash = 0;
+  for (char &c: key) {
+    int ascii = c;
+    hash += ascii;
+  }
+  return hash;
+}
+
+void set(string key, int value) {
+  int hash = make_hash(key);
+  *(start_ptr + hash) = value;
+  return;
+}
+
+int get(string key) {
+  int hash = make_hash(key);
+  return *(start_ptr + hash);
+}
+
+void remove(string key) {
+  int hash = make_hash(key);
+  *(start_ptr + hash) = 0;
+  return;
+}
+
+int main() {
+  set("yo", 50);
+  cout << get("y") << endl;
+  delete[] start_ptr;
+  return 0;
+}
